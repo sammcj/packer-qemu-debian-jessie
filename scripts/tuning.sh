0bf6e6453b6eb8edd998412f90c1e69c2ebb41bb
Sam McLeod <smj@fastmail.com>
2015-03-13 14:59:21 +1100

    swap zram init script for systemd unit

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -4,6 +4,6 @@
 echo '"ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="deadline"' >> /etc/udev/rules.d/60-schedulers.rules
 
 # Enable ZRAM
-mv /home/packer/zram /etc/init.d/zram
-chmod +x /etc/init.d/zram
-insserv zram
+mv /home/packer/zramst* /usr/local/bin/
+mv zram.service /etc/system/zram.service
+systemctl enable zram && systemctl start zram
